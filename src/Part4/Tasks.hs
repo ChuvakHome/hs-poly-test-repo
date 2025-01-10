4124e9063c0d8605a8b34f8887b8fd20e0a98863
timurdemenev <311683@niuitmo.ru>
2025-01-10 22:01:04 +0400

    Add impl for showsPrec, Update impl for type classes

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -23,27 +23,35 @@ listToRlist lst =
 
 -- Реализуйте все представленные ниже классы (см. тесты)
 instance Show a => Show (ReverseList a) where
-    showsPrec = notImplementedYet
+    showsPrec d = showsPrec d . rlistToList
     show = show . rlistToList
 instance Eq a => Eq (ReverseList a) where
---    (==) REmpty REmpty = True
---    (==) _ REmpty = False
---    (==) REmpty _ = False
---    (==) (xs :< x) (ys :< y) = x == y && xs == ys
-    xs == ys = rlistToList xs == rlistToList ys
+    (==) REmpty REmpty = True
+    (==) _ REmpty = False
+    (==) REmpty _ = False
+    (==) (xs :< x) (ys :< y) = x == y && xs == ys
+--    xs == ys = rlistToList xs == rlistToList ys
+--    (xs :< x) == (ys :< y) = x == y && xs == ys
 --    (/=) = notImplementedYet
 instance Semigroup (ReverseList a) where
-    xs <> ys = listToRlist $ (++) (rlistToList xs) (rlistToList ys)
+--    xs <> ys = listToRlist $ (++) (rlistToList xs) (rlistToList ys)
+    REmpty <> xs = xs
+    xs <> REmpty = xs
+    xs <> (ys :< y) = (xs <> ys) :< y
 instance Monoid (ReverseList a) where
     mempty = REmpty
-    xs `mappend` ys = listToRlist $ (++) (rlistToList xs) (rlistToList ys)
+--    xs `mappend` ys = listToRlist $ (++) (rlistToList xs) (rlistToList ys)
+    xs `mappend` ys = xs <> ys
 instance Functor ReverseList where
-    fmap f = listToRlist . map f . rlistToList
+--    fmap f = listToRlist . map f . rlistToList
+    fmap f REmpty = REmpty
+    fmap f (xs :< x) = (fmap f xs) :< (f x)
 instance Applicative ReverseList where
     pure a = REmpty :< a
-    REmpty <*> _ = REmpty
+    REmpty <*> xs = REmpty
     _ <*> REmpty = REmpty
-    xs <*> ys = listToRlist $ (<*>) (rlistToList xs) (rlistToList ys)
+--    xs <*> ys = listToRlist $ (<*>) (rlistToList xs) (rlistToList ys)
+    (fs :< f) <*> xs = (fs <*> xs) `mappend` (fmap f xs)
 instance Monad ReverseList where
     REmpty >>= _ = REmpty
     (xs :< x) >>= f = (xs >>= f) `mappend` (f x)
